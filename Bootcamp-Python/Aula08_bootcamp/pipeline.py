7c64d2876fb368d071d5fe1fc6800c30d5bf7c90
Matheus Sobreira <matheussobreira2702@gmail.com>
2025-03-31 19:23:10 -0300

    removendo aulas do bootcamp

--- Bootcamp-Python/Aula08_bootcamp/pipeline.py
+++
@@ -1,8 +0,0 @@
-from etl import pipeline_calculo_de_vendas
-
-
-pasta_argumento = r'C:\Users\Estudos\Desktop\DATA ENGINEER\01 - Python\Python-Estudos\Bootcamp-Python\Aula08_bootcamp\data' # Caminho onde está os arquivos JSON
-formato: list = ['csv', 'parquet']
-
-pipeline_calculo_de_vendas(pasta=pasta_argumento, formato_de_saida=formato)
-
